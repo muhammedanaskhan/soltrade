64491a5b5de2caa0b189df1a76e8baf58d41f12b
Anas <muhammedanas5186@gmail.com>
2024-08-03 15:15:24 +0530

    base project

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Topbar = () => {
+    return (
+        <div className='p-4 flex justify-between'>
+            <span className="text-[24px] font-bold">soltrade</span>
+        </div>
+    )
+}
+
+export default Topbar
